689a2930563d4e482889e62fc5f03833f4b8bad5
Michael Causey <michaelhcausey@gmail.com>
2020-05-19 21:12:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,6 @@ require 'pry'
 def reverse_each_word(string)
   new_array []
   string.split(" ").collect do |words|
-   new_array = new_array.concat(words.reverse)
+   word.reverse
   end
-  new_array.join(" ")
 end
